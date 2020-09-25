5c1543f41d704700a13603fdac9cd4d334de2f7e
SunStake <sunstake@protonmail.com>
2020-09-25 02:58:12 +0000

    Update website

---
+++ precache-manifest.7aa4abf0321f9f408ee4b444dd65a481.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c562f4a4907fb38d71897d41f7f62cfb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2f60a3e9848a8a88530",
+    "url": "/static/css/main.a51f8e49.chunk.css"
+  },
+  {
+    "revision": "a1e7171e3488ac597f1f",
+    "url": "/static/js/2.77e29939.chunk.js"
+  },
+  {
+    "revision": "e8b0ae1bd2f59a85f300b3c60c50941a",
+    "url": "/static/js/2.77e29939.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2f60a3e9848a8a88530",
+    "url": "/static/js/main.569119c6.chunk.js"
+  },
+  {
+    "revision": "d2a6031eaf941e643a31",
+    "url": "/static/js/runtime-main.e89a29cd.js"
+  },
+  {
+    "revision": "00498cbb7f564273cc20115d46eeb0ce",
+    "url": "/static/media/galaxy.00498cbb.png"
+  },
+  {
+    "revision": "ddc250127189a3b4ee4802e6c2493757",
+    "url": "/static/media/github.ddc25012.svg"
+  },
+  {
+    "revision": "204c38fd9ac73033c3bd0638dde468de",
+    "url": "/static/media/logo-header.204c38fd.png"
+  },
+  {
+    "revision": "02a226dd133504d0b478c01e0a56ae06",
+    "url": "/static/media/medium.02a226dd.svg"
+  },
+  {
+    "revision": "705a940e6b8b38ecc66bc1ebe94a6dc5",
+    "url": "/static/media/plus.705a940e.svg"
+  },
+  {
+    "revision": "0fcc8ec4c1efcdfd1f207c155d5da86d",
+    "url": "/static/media/slash.0fcc8ec4.svg"
+  },
+  {
+    "revision": "be73024e7167682e9e792deb4dc2ad88",
+    "url": "/static/media/swap.be73024e.svg"
+  },
+  {
+    "revision": "b4971d60d80ec71eec896e6b68b21bea",
+    "url": "/static/media/telegram.b4971d60.svg"
+  },
+  {
+    "revision": "76c75acc2e2b1d5e2cbbc2736af3cb1c",
+    "url": "/static/media/twitter.76c75acc.svg"
+  }
+]);
